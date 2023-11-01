f1261fd84511f8634ccdfc09222e52b1b02764dd
OlesiaManina <113991075+OlesiaManina@users.noreply.github.com>
2023-11-01 13:03:14 +0200

    HW before cheking

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,8 +1,11 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
+type Common = {
+  [key: string]: string | number | string[] | object;
+}
 
-const page1 = {
+const page1: Common = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,7 +16,7 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2: Common = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
